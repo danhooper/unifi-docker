d34a1ec8f4e77b7c83753ebf7a5c2b052bbc23a4
Dan Hooper <dan.c.hooper@gmail.com>
2021-01-17 13:44:20 -0500

    Different cert mount

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
-# cp /etc/letsencrypt/live/dhoops.dev/* /home/dhooper/unifi/cert/
 docker pull jacobalberty/unifi:stable
 docker stop unifi
 docker update --restart=no unifi
 docker rename unifi unifi-`date -u +"%Y-%m-%dT%H%M%SZ"`
 docker run --net=host --restart=always --init -d -p 8080:8080 -p 8443:8443 -p 3478:3478/udp -p 10001:10001/udp -e TZ='America/New_York' -e RUNAS_UID0=false \
-    -v ~/unifi:/unifi --name unifi jacobalberty/unifi:stable
+    -v ~/unifi:/unifi \
+	-v /volume1/certs/dhoops.dev:/unifi/cert \
+	--name unifi jacobalberty/unifi:stable
 
